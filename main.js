7f4a3280f00229262c5ba938c5d235e8876083a4
Javascript Enthusiast <abmercy035@gmail.com>
2023-04-13 21:00:59 +0100

    minor
    removed the script code in the index file and moved it to the main.js file

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('hello')
+const menu = document.getElementById('site-mobile-nav-bar');
+const sideNav = document.getElementById('aside-nav');
+const close = document.getElementById('close-icon');
+
+menu.addEventListener("click", () => {
+ sideNav.style.width = "200px"
+})
+close.addEventListener("click", () => {
+ sideNav.style.width = "0"
+})
